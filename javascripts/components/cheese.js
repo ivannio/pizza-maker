25614716563a376578a295131985a81630fa6e24
Ivan Phelps <ivanniothemannio@gmail.com>
2019-09-24 19:56:03 -0500

    added many cheeses

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -1,8 +1,26 @@
 import utilities from "../helpers/utilities.js";
 
+const cheeses = [
+    {id:"cheese1", name: "Swiss", price: 50},
+    {id:"cheese2", name: "American", price: 40},
+    {id:"cheese3", name: "Pepper Jack", price: 50},
+    {id:"cheese4", name: "Gorgonzola", price: 60},
+    {id:"cheese5", name: "Goat", price: 70},
+    {id:"cheese6", name: "Smoked Gouda", price: 80},
+    {id:"cheese7", name: "Bleu", price: 60},
+    {id:"cheese8", name: "Mozzarella", price: 50},
+    {id:"cheese9", name: "Colby Jack", price: 40},
+    {id:"cheese10", name: "Ricotta", price: 70},
+    {id:"cheese11", name: "Parmesan", price: 50}
+];
 const cheesePrinter = () => {
     let stringToPrint = '';
-    stringToPrint += "cheese";
+    for (let i = 0; i < cheeses.length; i++) {
+        stringToPrint += `<div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="${cheeses[i].id}">
+        <label class="form-check-label" for="exampleCheck1">${cheeses[i].name}</label>
+      </div>`;
+    }    
     utilities.printToDom(stringToPrint, "cheese-counter");
 }
 
